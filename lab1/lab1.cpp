e2f676b06ebd9a2ee04eabfcfc1c4811ca51ae01
ArtsemiLasyi <50248368+ArtsemiLasyi@users.noreply.github.com>
2020-09-16 09:08:48 +0300

    Обработка нажатий клавиш

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -11,6 +11,27 @@ HINSTANCE hInst;                                // текущий экземпл
 WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
 WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
 
+// Мои переменные:
+const int delta = 5;
+RECT wndRect;
+HBITMAP hBmp;
+BITMAP bmp;
+BOOL isMoving = false;
+struct figureInfo
+{
+    int x;
+    int y;
+    int width;
+    int height;
+} smile;
+
+// Объявление функций
+void InitSmile(figureInfo* smile, int x, int y, int width, int height);
+
+void MoveKeyboard(HWND hWnd, figureInfo* smile, WPARAM wParam, LPARAM lParam, int delta);
+
+void MoveMouse(HWND hWnd, figureInfo* smile, WPARAM wParam, LPARAM lParam);
+
 // Отправить объявления функций, включенных в этот модуль кода:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
@@ -75,7 +96,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hInstance      = hInstance;
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LAB1));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+2);
     wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_LAB1);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_LAB1));
@@ -125,23 +146,128 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
+        case WM_CREATE:
+        {
+            GetClientRect(hWnd, &wndRect);
+
+            hBmp = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP1));
+            GetObject(hBmp, sizeof(bmp), &bmp);
+            if (hBmp == NULL)
+                MessageBox(hWnd, L"Невозможно загрузить изображение!", L"Ошибка", MB_OK | MB_ICONERROR);
+          
+            InitSmile(&smile, wndRect.right / 2, wndRect.bottom / 2, 100, 100);
+        }
+        break;
+
         case WM_PAINT:
         {
+            HBITMAP hPrevBmp;
+            HDC hMemDc;
             PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
+
+            GetClientRect(hWnd, &wndRect);
+            HDC hWndDc = BeginPaint(hWnd, &ps);
+            hMemDc = CreateCompatibleDC(hWndDc);
+            hPrevBmp = (HBITMAP)SelectObject(hMemDc, hBmp);
+            smile.width = bmp.bmWidth;
+            smile.height = bmp.bmHeight;
+
+            BitBlt(hWndDc, smile.x - smile.width / 2, smile.y - smile.height / 2, bmp.bmWidth, bmp.bmHeight,
+                hMemDc, 0, 0, SRCAND);
+            SelectObject(hMemDc, hBmp);
+
+            DeleteDC(hMemDc);
+            DeleteObject(hPrevBmp);
+
             EndPaint(hWnd, &ps);
         }
         break;
 
-        case WM_DESTROY:
+        case WM_LBUTTONDOWN:
         {
-            PostQuitMessage(0);
+            POINT ptMousePos = { LOWORD(lParam), HIWORD(lParam) };
+            isMoving = true;
+            InvalidateRgn(hWnd, NULL, TRUE);
         }
         break;
+
+        case WM_MOUSEMOVE:
+        {
+            MoveMouse(hWnd, &smile, wParam, lParam);
+        }
+        break;
+
+        case WM_LBUTTONUP:
+        {
+            isMoving = false;
+        }
+        break;
+
+        case WM_KEYDOWN:
+        {
+            MoveKeyboard(hWnd, &smile, wParam, lParam, delta);
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        break;
+
+        case WM_MOUSEWHEEL:
+        {
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        break;
+
+        case WM_DESTROY:
+            DeleteObject(hBmp);
+            PostQuitMessage(0);
 
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
+}
+
+
+
+void InitSmile(figureInfo *smile, int x, int y, int width, int height)
+{
+    smile->x = x;
+    smile->y = y;
+    smile->height = height;
+    smile->width = width;
+}
+
+void MoveMouse(HWND hWnd, figureInfo* smile, WPARAM wParam, LPARAM lParam)
+{
+    if (isMoving) {
+        smile->x = LOWORD(lParam);
+        smile->y = HIWORD(lParam);
+        InvalidateRect(hWnd, NULL, TRUE);
+    }
+}
+
+void MoveKeyboard(HWND hWnd, figureInfo* smile, WPARAM wParam, LPARAM lParam, int delta)
+{
+    switch (LOWORD(wParam))
+    {
+        case VK_LEFT:
+            smile->x -= delta;
+        break;
+
+        case VK_UP:
+            smile->y -= delta;
+        break;
+
+        case VK_RIGHT:
+            smile->x += delta;
+        break;
+
+        case VK_DOWN:
+            smile->y += delta;
+        break;
+    }
+}
+
+void MoveMouseWheel()
+{
+
 }
--- lab1/resource.h
+++ lab1/resource.h
@@ -11,6 +11,7 @@
 #define IDI_LAB1                        107
 #define IDC_LAB1                        109
 #define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     132
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -18,7 +19,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
